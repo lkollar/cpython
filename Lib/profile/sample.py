3bd85a8e3c68c36ce4a2c31bd96f87c048892d45
László Kiss Kollár <kiss.kollar.laszlo@gmail.com>
2025-06-07 08:57:00 +0100

    fixup! fixup! Move core profiling module into a package

--- Lib/profile/sample.py
+++ Lib/profile/sample.py
@@ -1,19 +1,25 @@
 import collections
+import marshal
 import pstats
 import time
 import _remote_debugging
 import argparse
+
 
 class SampleProfile:
     def __init__(self, pid, sample_interval_usec, all_threads):
         self.pid = pid
         self.sample_interval_usec = sample_interval_usec
         self.all_threads = all_threads
-        self.unwinder = _remote_debugging.RemoteUnwinder(self.pid, all_threads=self.all_threads)
+        self.unwinder = _remote_debugging.RemoteUnwinder(
+            self.pid, all_threads=self.all_threads
+        )
         self.stats = {}
 
     def sample(self, duration_sec=10):
-        result = collections.defaultdict(lambda: dict(total_calls=0, total_rec_calls=0, inline_calls=0))
+        result = collections.defaultdict(
+            lambda: dict(total_calls=0, total_rec_calls=0, inline_calls=0)
+        )
         sample_interval_sec = self.sample_interval_usec / 1_000_000
 
         running_time = 0
@@ -34,14 +40,16 @@ class SampleProfile:
             running_time = time.perf_counter() - start_time
 
         print(f"Captured {num_samples} samples in {running_time:.2f} seconds")
-        print(f"Sample rate: {num_samples/running_time:.2f} samples/sec")
-        print(f"Error rate: {(errors/num_samples)*100:.2f}%")
+        print(f"Sample rate: {num_samples / running_time:.2f} samples/sec")
+        print(f"Error rate: {(errors / num_samples) * 100:.2f}%")
 
         expected_samples = int(duration_sec / sample_interval_sec)
         if num_samples < expected_samples:
-            print(f"Warning: missed {expected_samples - num_samples} samples "
+            print(
+                f"Warning: missed {expected_samples - num_samples} samples "
                 f"from the expected total of {expected_samples} "
-                f"({(expected_samples - num_samples)/expected_samples*100:.2f}%)")
+                f"({(expected_samples - num_samples) / expected_samples * 100:.2f}%)"
+            )
 
         self.stats = self.convert_to_pstats(result)
 
@@ -51,7 +59,7 @@ class SampleProfile:
         pstats.SampledStats(self).strip_dirs().sort_stats(*sort).print_stats()
 
     def dump_stats(self, file):
-        with open(file, 'wb') as f:
+        with open(file, "wb") as f:
             marshal.dump(self.stats, f)
 
     # Needed for compatibility with pstats.Stats
@@ -67,10 +75,12 @@ class SampleProfile:
             cumulative = call_counts["total_calls"] * sample_interval_sec
             pstats[fname] = (
                 call_counts["total_calls"],
-                call_counts["total_rec_calls"] if call_counts["total_rec_calls"] else call_counts["total_calls"],
+                call_counts["total_rec_calls"]
+                if call_counts["total_rec_calls"]
+                else call_counts["total_calls"],
                 total,
                 cumulative,
-                callers, # FIXME this is most certainly broken
+                callers,  # FIXME this is most certainly broken
             )
 
         return pstats
@@ -90,7 +100,9 @@ class SampleProfile:
 
             if len(frames) > 1:
                 next_frame_loc = frames[1]
-                callers[top_location][next_frame_loc] = callers[top_location].get(next_frame_loc, 0) + 1
+                callers[top_location][next_frame_loc] = (
+                    callers[top_location].get(next_frame_loc, 0) + 1
+                )
             else:
                 continue
 
@@ -100,8 +112,9 @@ class SampleProfile:
                     result[location]["total_rec_calls"] += 1
 
 
-
-def sample(pid, *, sort=-1, sample_interval_usec=100, duration_sec=10, filename=None):
+def sample(
+    pid, *, sort=-1, sample_interval_usec=100, duration_sec=10, filename=None
+):
     profile = SampleProfile(pid, sample_interval_usec, all_threads=False)
     profile.sample(duration_sec)
     if filename:
@@ -109,20 +122,36 @@ def sample(pid, *, sort=-1, sample_interval_usec=100, duration_sec=10, filename=
     else:
         profile.print_stats(sort)
 
+
 def main():
-    parser = argparse.ArgumentParser(description="Sample a process's stack frames.", color=True)
+    parser = argparse.ArgumentParser(
+        description="Sample a process's stack frames.", color=True
+    )
     parser.add_argument("pid", type=int, help="Process ID to sample.")
     parser.add_argument(
-        "-i", "--interval", type=int, default=10,
-        help="Sampling interval in microseconds (default: 10 usec)."
+        "-i",
+        "--interval",
+        type=int,
+        default=10,
+        help="Sampling interval in microseconds (default: 10 usec).",
     )
     parser.add_argument(
-        "-d", "--duration", type=int, default=10,
-        help="Sampling duration in seconds (default: 10 seconds)."
+        "-d",
+        "--duration",
+        type=int,
+        default=10,
+        help="Sampling duration in seconds (default: 10 seconds).",
     )
+    parser.add_argument("-o", "--outfile", help="Save stats to <outfile>")
     args = parser.parse_args()
 
-    sample(args.pid, sample_interval_usec=args.interval, duration_sec=args.duration)
+    sample(
+        args.pid,
+        sample_interval_usec=args.interval,
+        duration_sec=args.duration,
+        filename=args.outfile,
+    )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
